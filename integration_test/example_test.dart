c7ca5254c4d5347fe9eaff84dd08bbdc1820fca3
Atanas Chanev <atanas.chanev@bitrise.io>
2023-05-02 11:12:07 +0100

    Added pump and settle to test

--- integration_test/example_test.dart
+++ integration_test/example_test.dart
@@ -7,6 +7,9 @@ void main() {
 
   testWidgets("Succeeding test example", (WidgetTester tester) async {
     app.main();
+
+    await tester.pumpAndSettle();
+    
     expect(2 + 2, equals(4));
   });
 }
